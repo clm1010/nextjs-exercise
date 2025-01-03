5e2a8027a2be04777eda902b7a3d746497ebce9e
clm <limin_chu@163.com>
2025-01-04 02:09:52 +0800

    分析退出请求记忆的表现

--- my-app/src/app/a/page.tsx
+++ my-app/src/app/a/page.tsx
@@ -3,11 +3,17 @@ import Image from 'next/image'
 // import { headers } from 'next/headers'
 
 const fetchImg = async () => {
+  // Request Memoization 请求记忆
+  // 通过 AbortController 退出请求记忆
+  // 记忆化仅适用于获取请求中的 GET 方法，其他方法（例如 POST 和 DELETE）不会被记忆化。
+  // 此默认行为是 React 的优化，我们不建议选择退出。
+  const { signal } = new AbortController()
   const response = await fetch('https://dog.ceo/api/breeds/image/random', {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json'
     },
+    signal, // 退出请求记忆，每次请求的数据都不一样，虽然请求了三次，但是请求的图片都是不一样的
     // next.js 15.0.0 版本 开发环境下开始支持 fetch cache:'no-store' 默认不缓存，可以通过设置 cache: 'force-cache' 强制缓存
     // 但是生产环境下不支持，所以需要设置 cache: 'no-store' 不缓存
     cache: 'no-store'
