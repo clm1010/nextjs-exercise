7acb7a93de0c3597861e327460badc15c20385d0
clm <limin_chu@163.com>
2025-01-05 16:11:46 +0800

    清除客户端路由缓存1

--- my-app/src/app/(cache)/layout.tsx
+++ my-app/src/app/(cache)/layout.tsx
@@ -1,8 +1,6 @@
-import Link from 'next/link'
-
-// 使用 dynamic = 'force-dynamic' 或  revalidate = 0 路由段配置选项：这将跳过完整路由缓存和数据缓存
-// 表示强制动态渲染，不使用缓存
-export const dynamic = 'force-dynamic'
+'use client'
+// import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 
 /**
  *
@@ -14,16 +12,29 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode
 }>) {
+  // 使用 useRouter 跳转路由
+  const router = useRouter()
+  const handlerClickNews = () => {
+    router.push('/news')
+    router.refresh()
+  }
+
+  const handlerClickSports = () => {
+    router.push('/sports')
+    router.refresh()
+  }
   return (
     <div className='container mx-auto flex justify-center gap-4 h-screen items-center flex-col'>
       <nav className='flex gap-4'>
         {/* prefetch={false} 禁用预加载 */}
-        <Link className='text-blue-600 hover:underline' href='/news'>
+        {/* <Link className='text-blue-600 hover:underline' href='/news'>
           新闻
         </Link>
         <Link className='text-blue-600 hover:underline' href='/sports' >
           体育
-        </Link>
+        </Link> */}
+        <span className='text-blue-600 hover:underline cursor-pointer' onClick={handlerClickNews}>新闻</span>
+        <span className='text-blue-600 hover:underline cursor-pointer' onClick={handlerClickSports}>体育</span>
       </nav>
       {children}
     </div>
--- my-app/src/app/(cache)/news/page.tsx
+++ my-app/src/app/(cache)/news/page.tsx
@@ -1,5 +1,7 @@
 import { sleep } from '@/utils'
-import React from 'react'
+// 使用 dynamic = 'force-dynamic' 或  revalidate = 0 路由段配置选项：这将跳过完整路由缓存和数据缓存
+// 表示强制动态渲染，不使用缓存
+export const dynamic = 'force-dynamic'
 
 export default async function Page() {
   await sleep(3000)
