da7c7c0867ceef395c7a1b03d6b7eb630e938c83
clm <limin_chu@163.com>
2025-01-06 11:18:44 +0800

    案例 Server Action 演示及注意事项

---
+++ my-app/src/actions/index.ts
@@ -0,0 +1,60 @@
+'use server'
+
+// import { revalidatePath, revalidateTag } from 'next/cache'
+import { revalidatePath } from 'next/cache'
+
+// const data = ['吃饭', '睡觉', '打豆豆']
+const data = [
+  { id: 1, todo: '吃饭' },
+  { id: 2, todo: '睡觉' },
+  { id: 3, todo: '打豆豆' }
+]
+
+/**
+ * @description 获取待办事项
+ * @returns data
+ */
+export async function getTodos() {
+  return data
+}
+
+/**
+ * @description 添加待办事项
+ * @description 例子：如果需要结合表单传递其他数据
+ * @param userId
+ * @param formData
+ */
+export async function addTodo(userId: string, formData: FormData) {
+  // 通过 Object.fromEntries 获取 formData 表单 全部数据
+  const rawFormData = Object.fromEntries(formData)
+  // $ACTION_ID_ 区分不同的表单
+  console.log(rawFormData)
+  console.log(userId, '🐮')
+  const todo = formData.get('todo') as string
+  // data.push(todo)
+  data.push({ id: data.length + 1, todo })
+  console.log(data)
+  // 您可以使用 revalidatePath 路径方式 重新验证
+  revalidatePath('/')
+  // 或者使用 revalidateTag  标签方式 重新验证
+  // revalidateTag('todos')
+}
+
+/**
+ * @description 添加待办事项
+ * @param formData
+ */
+// export async function addTodo(formData: FormData) {
+//   // 通过 Object.fromEntries 获取 formData 表单 全部数据
+//   const rawFormData = Object.fromEntries(formData)
+//   // $ACTION_ID_ 区分不同的表单
+//   console.log(rawFormData)
+//   const todo = formData.get('todo') as string
+//   // data.push(todo)
+//   data.push({ id: data.length + 1, todo })
+//   console.log(data)
+//   // 您可以使用 revalidatePath 路径方式 重新验证
+//   revalidatePath('/')
+//   // 或者使用 revalidateTag  标签方式 重新验证
+//   // revalidateTag('todos')
+// }
--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,61 +1,66 @@
-'use client'
-import React, { useEffect, useState } from 'react'
+import { addTodo, getTodos } from '@/actions'
 
-export default function Page() {
-  const [todos, setTodos] = useState([])
+// 例子：如果需要结合表单传递其他数据
+const userId = '888'
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await fetch('http://localhost:3000/api/todos', {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      })
-      const { data } = await res.json()
-      console.log(data)
-      setTodos(data)
-    }
-    fetchData()
-  }, [])
-
-  const handlerSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    const res = await fetch('http://localhost:3000/api/todos', {
-      method: 'POST',
-      // headers: {
-      //   'Content-Type': 'multipart/form-data;'
-      // },
-      body: new FormData(e.currentTarget)
-    })
-
-    const { data } = await res.json()
-    setTodos(data)
-    console.log(data)
-  }
+export default async function Page() {
+  const todos = await getTodos()
+  // 例子:如果需要结合表单传递其他数据 第二种写法
+  const addTodoWithOther = addTodo.bind(null, userId)
   return (
     <div className='container mx-auto flex flex-col items-center justify-center h-screen'>
+      {/* 例子:如果需要结合表单传递其他数据 第二种写法 */}
       <form
         className='flex flex-col border border-gray-300 p-4 gap-2 rounded-md'
-        onSubmit={handlerSubmit}
+        action={addTodoWithOther}
       >
+        {/* 例子:如果需要结合表单传递其他数据 第一种写法 */}
+        {/* <form
+        className='flex flex-col border border-gray-300 p-4 gap-2 rounded-md'
+        action={async (formData) => {
+          'use server'
+          await addTodo(userId, formData)
+        }}
+      > */}
+        {/* React 拓展了 form 标签action的能力，可以给action指定一个 Server Action */}
+        {/* 第一种写法 action={addTodo} */}
+        {/* <form
+        className='flex flex-col border border-gray-300 p-4 gap-2 rounded-md'
+        action={addTodo}
+      > */}
         <input
           className='border border-gray-300 rounded-md'
           type='text'
           name='todo'
           placeholder='请输入待办事项'
         />
+        <input
+          className='border border-gray-300 rounded-md'
+          type='Password'
+          name='pwd'
+          placeholder='Password'
+        />
+        {/* 第二种写法 formAction={addTodo} */}
         <button
           className='border border-gray-300 hover:bg-blue-600 hover:text-white rounded-md'
           type='submit'
+          // formAction={addTodo}
         >
           提交
         </button>
       </form>
       <ul className='leading-8 mt-4 border border-gray-300 p-2 w-800 rounded-md'>
-        {todos.map((todo, index) => (
-          <li className='border-b border-gray-200 w-full' key={index}>
+        {/* {todos.map((todo, index) => (
+          <li key={index} className='border-b border-gray-200 w-full'>
             {todo}
+          </li>
+        ))} */}
+        {todos.map((item, index) => (
+          <li
+            key={item.id || index}
+            className='border-b border-gray-200 w-full'
+          >
+            {item.todo}
           </li>
         ))}
       </ul>
