891b3b9d066e9cedabe0b12b0212a4123cba4147
clm <limin_chu@163.com>
2025-01-06 17:45:03 +0800

    nextjs综合案例初始

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -3,17 +3,17 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true, // 严格模式 true 开启 false 关闭，当关闭后开发阶段不会出现两次打印
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'i.ibb.co'
-        // port: '',
-        // pathname: '',
-        // search: ''
-      }
-    ]
-  }
+  // images: {
+  //   remotePatterns: [
+  //     {
+  //       protocol: 'https',
+  //       hostname: 'i.ibb.co'
+  //       // port: '',
+  //       // pathname: '',
+  //       // search: ''
+  //     }
+  //   ]
+  // }
 }
 
 export default nextConfig
